9a506a9a1c8316df0b5e496c9f53cda7cfe58dfe
Bhanuprakash0531 <bhanuprakash45.y@gmail.com>
2025-04-01 20:31:02 -0500

    blog controller updated

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,12 +1,17 @@
 import pool from '../db.js';
 
 export  const  createBlogPost =async(req,res)=>{
-    const userId=1;
+    //const userId=1;
+    const {title,body,email}=req.body;
     try{
-        const {title,body}=req.body;
-        if(!title || !body){
-            return res.status(400).json({error: 'Missing title or body'});
+        if(!title || !body || !email){
+            return res.status(400).json({error: 'Missing title,body or email'});
         }
+        const userResult= await pool.query('SELECT id FROM users WHERE email = $1',[email]);
+        if(userResult.rows.length === 0){
+            return res.status(404).json({error: 'User not found'});
+        }
+        const userId=userResult.rows[0].id;
         const result=await pool.query(`INSERT INTO blogs (user_id,title,content) Values ($1,$2,$3) RETURNING *`,[userId,title,body]);
         res.json(result.rows[0]);
     }catch(error){
